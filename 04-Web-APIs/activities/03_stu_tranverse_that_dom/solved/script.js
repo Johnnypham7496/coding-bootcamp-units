ba1b5b539f4e39a4ab9132732bbfb4ed40784007
Johnnpham7496 <johnpham7496@gmail.com>
2023-11-29 16:13:18 -0600

    solved first half of manipulating article div

---
+++ 04-Web-APIs/activities/03_stu_tranverse_that_dom/solved/script.js
@@ -0,0 +1,6 @@
+var artDiv = document.getElementById("articles");
+var mainDiv = document.getElementById("main");
+
+artDiv.children[0].lastElementChild.computedStyleMap.color = "blue";
+artDiv.children[0].style.fontSize ="50px";
+artDiv.previousElementSibling.style.background = "black";
